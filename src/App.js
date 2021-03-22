0c0976ba5d9809bf987bb868576d2b329710cb6a
Simone Odegard <Simone.Odegard@gmail.com>
2021-03-22 16:50:01 -0700

    updated key

--- src/App.js
+++ src/App.js
@@ -14,13 +14,13 @@ class App extends React.Component{
 
   getLocationInfo = async(e) => {
     e.preventDefault();
-    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_key}&q=${this.state.searchQuery}&format=json`;
+    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.key}&q=${this.state.searchQuery}&format=json`;
     const location = await axios.get(url);
     const locationArray = location.data;
     this.setState({
       location: locationArray[0],
       displayResults: true,
-      imgSrc: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_key}&center=${locationArray[0].lat},${locationArray[0].lon}&zoom=13`
+      imgSrc: `https://maps.locationiq.com/v3/staticmap?key=${process.env.key}&center=${locationArray[0].lat},${locationArray[0].lon}&zoom=13`
     });
   }
 
@@ -29,7 +29,7 @@ class App extends React.Component{
       <>
         <form onSubmit={this.getLocationInfo} >
           <input onChange={(e) => this.setState({ searchQuery: e.target.value})} placeholder="city"/>
-          <button type = "submit"> search</button>
+          <button type = "submit"> Explore!</button>
         </form>
         <h1>Welcome!</h1>
         {this.state.displayResults &&
