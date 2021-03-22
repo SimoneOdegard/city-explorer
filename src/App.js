871176e848246f05221ae316b4b47a11f4a70b46
Simone Odegard <Simone.Odegard@gmail.com>
2021-03-22 16:40:03 -0700

    updated app.js and added key

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
+import React from 'react';
+import axios from 'axios';
 
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      location:{},
+      searchQuery: '',
+      imgSrc: '',
+      displayResults: false
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>test</h1>
-      </header>
-    </div>
-  );
+  getLocationInfo = async(e) => {
+    e.preventDefault();
+    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_key}&q=${this.state.searchQuery}&format=json`;
+    const location = await axios.get(url);
+    const locationArray = location.data;
+    this.setState({
+      location: locationArray[0],
+      displayResults: true,
+      imgSrc: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_key}&center=${locationArray[0].lat},${locationArray[0].lon}&zoom=13`
+    });
+  }
+
+  render(){
+    return(
+      <>
+        <form onSubmit={this.getLocationInfo} >
+          <input onChange={(e) => this.setState({ searchQuery: e.target.value})} placeholder="city"/>
+          <button type = "submit"> search</button>
+        </form>
+        <h1>Welcome!</h1>
+        {this.state.displayResults &&
+          <>
+            <h2>{this.state.location.display_name}</h2>
+            <img src={this.state.imgSrc} />
+          </>
+        }
+      </>
+    )
+  }
 }
 
 export default App;
